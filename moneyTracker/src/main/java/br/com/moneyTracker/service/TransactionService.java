456ad6f174340ff6803e39e6399c3a11038df98d
educoelho03 <eduardopaivacoelho@outlook.com>
2025-04-30 15:24:45 -0300

    adding logs

--- moneyTracker/src/main/java/br/com/moneyTracker/service/TransactionService.java
+++ moneyTracker/src/main/java/br/com/moneyTracker/service/TransactionService.java
@@ -4,11 +4,13 @@ import br.com.moneyTracker.domain.entities.Transactions;
 import br.com.moneyTracker.domain.entities.User;
 import br.com.moneyTracker.domain.enums.TRANSACTION_TYPE;
 import br.com.moneyTracker.dto.response.TransactionResponseDTO;
-import br.com.moneyTracker.exceptions.InvalidTokenException;
 import br.com.moneyTracker.exceptions.SaldoInsuficienteException;
+import br.com.moneyTracker.exceptions.InvalidTransactionException;
 import br.com.moneyTracker.infra.security.TokenService;
 import br.com.moneyTracker.repository.TransactionRepository;
 import br.com.moneyTracker.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,6 +18,8 @@ import java.util.stream.Collectors;
 
 @Service
 public class TransactionService {
+
+    private static Logger logger = LoggerFactory.getLogger(TransactionService.class);
 
     private final TokenService tokenService;
     private final UserService userService;
@@ -30,17 +34,10 @@ public class TransactionService {
     }
 
     public List<TransactionResponseDTO> listTransactionsByEmail(String token) { // TODO: DUVIDA AQUI, É UMA BOA PRATICA FAZER A CONVERSAO DE ENTITY PARA RESPONSE DENTRO DA SERVICE?
-        if (token == null || token.isEmpty()) {
-            throw new InvalidTokenException("Invalid token");
-        }
-
         String userEmail = tokenService.validateToken(token);
-
-        if (userEmail == null || userEmail.isEmpty()) {
-            throw new InvalidTokenException("Invalid or expired token");
-        }
-
         User user = userService.findUserByEmail(userEmail);
+
+        logger.info("Listing transactions for user: {}", userEmail);
 
         return user.getTransactions().stream()
                 .map(transactions -> new TransactionResponseDTO(
@@ -56,34 +53,52 @@ public class TransactionService {
         String userEmail = tokenService.validateToken(token); // TODO: REVER ESSE PONTO
         User user = userService.findUserByEmail(userEmail);
 
-        // Associa a transação ao usuário
+        logger.info("Creating new transaction for user: {}", userEmail);
+
         transaction.setUser(user);
 
         calculateAmount(user, transaction);
         Transactions savedTransaction = transactionRepository.save(transaction);
         userRepository.save(user);
 
+        logger.info("Transaction created successfully with ID: {}", savedTransaction.getTransaction_id());
         return savedTransaction;
     }
 
     public void calculateAmount(User user, Transactions transaction) {
-        if (transaction.getTransactionType() == TRANSACTION_TYPE.DESPESA) {
-            subtractToBalance(user, transaction.getAmount());
-        } else if (transaction.getTransactionType() == TRANSACTION_TYPE.DEPOSITO) {
-            addToBalance(user, transaction.getAmount());
+        try {
+            if (transaction.getTransactionType() == TRANSACTION_TYPE.DESPESA) {
+                subtractToBalance(user, transaction.getAmount());
+            } else if (transaction.getTransactionType() == TRANSACTION_TYPE.DEPOSITO) {
+                addToBalance(user, transaction.getAmount());
+            }
+        } catch (SaldoInsuficienteException e) {
+            logger.error("Insufficient balance for user: {}", user.getEmail(), e);
+            throw e;
         }
     }
 
     public void addToBalance(User user, double amount){
+        if(amount <= 0) {
+            throw new InvalidTransactionException("Amount to add must be positive");
+        }
         double newBalance = user.getSaldo() + amount;
+        logger.info("Adding to balance for user: {}. amount: {}", user.getEmail(), amount);
         user.setSaldo(newBalance);
+        logger.info("Final balance for user: {}. amount: {}", user.getEmail(), user.getSaldo());
     }
 
     public void subtractToBalance(User user, double amount) {
+        if(amount <= 0) {
+            throw new InvalidTransactionException("Amount to subtract must be positive");
+        }
         if (amount > user.getSaldo()) {
             throw new SaldoInsuficienteException("Saldo insuficiente para realizar a transação.");
         }
         double newBalance = user.getSaldo() - amount;
+        logger.info("Subtracting to balance for user: {}. amount: {}", user.getEmail(), amount);
         user.setSaldo(newBalance);
+        logger.info("Final balance for user: {}. amount: {}", user.getEmail(), user.getSaldo());
+
     }
 }
--- moneyTracker/src/main/java/br/com/moneyTracker/service/UserService.java
+++ moneyTracker/src/main/java/br/com/moneyTracker/service/UserService.java
@@ -8,11 +8,17 @@ import br.com.moneyTracker.exceptions.UserAlreadyExistsException;
 import br.com.moneyTracker.exceptions.UserNotFoundException;
 import br.com.moneyTracker.interfaces.UserServiceInterface;
 import br.com.moneyTracker.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
 @Service
 public class UserService implements UserServiceInterface {
+
+    private Logger logger = LoggerFactory.getLogger(UserService.class);
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
@@ -24,16 +30,17 @@ public class UserService implements UserServiceInterface {
 
     @Override
     public void updateUserPassword(String email, String newPassword) {
-        if (email == null) {
-            throw new UserNotFoundException("user email cannot be null");
+        if (email.isEmpty()) {
+            throw new IllegalArgumentException("Email cannot be empty");
         }
         if (newPassword == null || newPassword.isEmpty()) {
-            throw new PasswordNullException("password cannot be null");
+            throw new PasswordNullException("Password cannot be null or empty");
         }
 
         User userRecovery = findUserByEmail(email);
+
         if(passwordEncoder.matches(newPassword, userRecovery.getPassword())) {
-            throw new SamePasswordException("Password must be different");
+            throw new SamePasswordException("Passwords must be different");
         }
         userRecovery.setPassword(passwordEncoder.encode(newPassword));
         userRepository.save(userRecovery);
@@ -41,17 +48,22 @@ public class UserService implements UserServiceInterface {
 
     @Override
     public User registerUser(AuthRegisterRequestDTO authRegisterRequestDTO) {
-        // Verifica se o email já existe
-        if (userRepository.findUserByEmail(authRegisterRequestDTO.email()).isPresent()) {
-            throw new UserAlreadyExistsException("User with this email: " + authRegisterRequestDTO.email() + " already exists");
+        String email = authRegisterRequestDTO.email().trim();
+        if (email.isEmpty()) {
+            throw new IllegalArgumentException("Email cannot be empty");
         }
 
-        // Se não existir, cria o novo usuário
+        userRepository.findUserByEmail(email)
+                .ifPresent(user -> {
+                    throw new UserAlreadyExistsException(
+                            String.format("User with email %s already exists", email));
+                });
+
         User newUser = createNewUser(authRegisterRequestDTO);
         return userRepository.save(newUser);
     }
 
-    public User findUserByEmail(String email) { // TODO: DUVIDA SOBRE COMO APLICAR O SOLID PARA REPOSITORIOS
+    public User findUserByEmail(String email) {
         return userRepository.findUserByEmail(email)
                 .orElseThrow(() -> new UserNotFoundException("User with this email : " + email + " not found"));
     }
